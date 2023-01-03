d93404e2c085baef6f162ca2a36086f011647088
Danyl Shkurak <dedpanas666@gmail.com>
2023-01-03 11:16:20 +0200

    change hibernate configur class

---
+++ src/main/java/homework26/util/HibernateConfiguration.java
@@ -0,0 +1,33 @@
+package homework26.util;
+
+
+import homework26.entity.Account;
+import homework26.entity.Client;
+import homework26.entity.Status;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateConfiguration {
+    private static SessionFactory sessionFactory;
+
+
+    public static SessionFactory getSessionFactory() {
+        {
+            if (sessionFactory == null) {
+                try {
+                    Configuration configuration = new Configuration().configure();
+                    configuration.addAnnotatedClass(Client.class);
+                    configuration.addAnnotatedClass(Status.class);
+                    configuration.addAnnotatedClass(Account.class);
+                    StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                    sessionFactory = configuration.buildSessionFactory(builder.build());
+
+                } catch (Exception e) {
+                    System.out.println("Исключение!" + e);
+                }
+            }
+            return sessionFactory;
+        }
+
+}}
